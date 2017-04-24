702f5c99b8a4d38406bf87d852750f8b2c3297ef
TaiseiIto <taisei1212@outlook.jp>
2017-04-24 22:38:47 +0900

    TaiseiIto edited ComputerEngineeringLaboratoryB/cel-B/my-reports/s153012-1.tex.

--- ComputerEngineeringLaboratoryB/cel-B/caesar/key.c
+++ ComputerEngineeringLaboratoryB/cel-B/caesar/key.c
@@ -8,12 +8,12 @@ int main(void)
 	int numbers[ASCIIMAX + 1];	//まだ出力されていない数の一覧
 	int i,j;	//ループ用
 	srand((unsigned int)time(NULL));
-	for(i = 0; i <= ASCIIMAX; i++)numbers[i] = i;
+	for(i = 0; i <= ASCIIMAX; i++)numbers[i] = i;//配列の初期化
 	for(i = 0; i <= ASCIIMAX; i++)
 	{
-		int number = rand() % (ASCIIMAX + 1 - i);//出力する数字
-		printf("%d\n",numbers[number]);
-		for(j = number; j < ASCIIMAX - i; j++)numbers[j] = numbers[j + 1];
+		int number = rand() % (ASCIIMAX + 1 - i);//出力する数字の配列上の位置を乱数で決める。
+		printf("%d\n",numbers[number]);//数字を出力する
+		for(j = number; j < ASCIIMAX - i; j++)numbers[j] = numbers[j + 1];//出力された数字より後の要素を全てひとつ前に移動させる。
 		numbers[ASCIIMAX - i] = 0;
 	}
 	return EXIT_SUCCESS;
