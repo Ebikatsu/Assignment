f5183bb277a52c871a59b930d17541e638d41304
TaiseiIto <taisei1212@outlook.jp>
2017-05-20 18:26:00 +0900

    TaiseiIto edited syspj3.

--- syspj3/kintone.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-firefox https://cis-shimane-u.cybozu.com > /dev/null 2>&1 &
-
---
+++ syspj3/web.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+firefox https://moodle.cerd.shimane-u.ac.jp/moodle/ > /dev/null 2>&1 &
+firefox https://cis-shimane-u.cybozu.com > /dev/null 2>&1 &
