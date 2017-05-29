4c330f3c31e9e2118fc48bfb04bedc71ddf2a109
TaiseiIto <taisei1212@outlook.jp>
2017-05-29 10:24:23 +0900

    TaiseiIto edited ExerciseOfFundamentalDataStructures/kiso_data/enshu7.

--- ExerciseOfFundamentalDataStructures/kiso_data/enshu7/7_run.sh
+++ ExerciseOfFundamentalDataStructures/kiso_data/enshu7/7_run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-for i in `ls fruits` ; do echo 線形探索 ; ./7_program31 < fruits/${i} ; echo 二分探索 ; ./7_program31 < fruits/${i} ; done
+for i in `ls fruits`
+do
+	echo 検索データ$i
+	echo 線形探索
+	./7_program31 < fruits/${i}
+	echo 二分探索
+	./7_program33 < fruits/${i}
+done
