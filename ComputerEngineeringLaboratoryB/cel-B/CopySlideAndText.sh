95de246b670fee7e149161582bf7f091d28903d8
TaiseiIto <taisei1212@outlook.jp>
2017-06-16 21:12:26 +0900

    TaiseiIto modified ComputerEngineeringLaboratoryB/cel-B/CopySlideAndText.sh.

--- ComputerEngineeringLaboratoryB/cel-B/CopySlideAndText.sh
+++ ComputerEngineeringLaboratoryB/cel-B/CopySlideAndText.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cp ~/cel-B/texts/lec${1}.pdf /media/s153012/PUBLIC/taisei/cel-B
 number=${1}
-test ${#1} -eq 1 ; number=0${number}
+if [ ${#1} -eq 1 ] ; then
+	number=0${number}
+fi
 cp ~/cel-B/slides/${number}.pdf /media/s153012/PUBLIC/taisei/cel-B
 
