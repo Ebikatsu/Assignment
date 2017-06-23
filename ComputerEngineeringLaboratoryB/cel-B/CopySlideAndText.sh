6ecada7e9c145289d1820f043615ff71843682e0
TaiseiIto <taisei1212@outlook.jp>
2017-06-23 17:46:31 +0900

    TaiseiIto edited ComputerEngineeringLaboratoryB/cel-B/*.sh.

--- ComputerEngineeringLaboratoryB/cel-B/CopySlideAndText.sh
+++ ComputerEngineeringLaboratoryB/cel-B/CopySlideAndText.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cp ~/cel-B/texts/lec${1}.pdf /media/s153012/PUBLIC/taisei/cel-B
+cp ~/cel-B/texts/lec${1}.pdf "/media/s153012/USB DISK/taisei/cel-B"
 number=${1}
 if [ ${#1} -eq 1 ] ; then
 	number=0${number}
 fi
-cp ~/cel-B/slides/${number}.pdf /media/s153012/PUBLIC/taisei/cel-B
+cp ~/cel-B/slides/${number}.pdf "/media/s153012/USB DISK/taisei/cel-B"
 
--- ComputerEngineeringLaboratoryB/cel-B/ReadTextUSBM.sh
+++ ComputerEngineeringLaboratoryB/cel-B/ReadTextUSBM.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-evince /media/taisei/PUBLIC/taisei/cel-B/lec${1}.pdf > /dev/null 2>&1 &
+evince "/media/taisei/USB DISK/taisei/cel-B/lec${1}.pdf" > /dev/null 2>&1 &
 
